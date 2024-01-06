37b383bce2dd8223cbb8be5a8ad96b8893712749
Bregwin Jogi <bregwinpaul@gmail>
2024-01-06 18:13:48 -0500

    lint....

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -186,8 +186,8 @@ const Home = function () {
 
       <section id="about" className="bg-white dark:bg-gray-900">
         <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
-          <img className="w-full dark:hidden" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup.svg" alt="dashboard image"/>
-            <img className="w-full hidden dark:block" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup-dark.svg" alt="dashboard image"/>
+          <Image className="w-full dark:hidden" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup.svg" alt="dashboard image"/>
+            <Image className="w-full hidden dark:block" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup-dark.svg" alt="dashboard image"/>
               <div className="mt-4 md:mt-0">
                 <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">How it works</h2>
                 <p className="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">
@@ -195,7 +195,7 @@ const Home = function () {
                 </p>
                 <ul className="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">
                   <li>1. Paste the YouTube video URL you want summarized.</li>
-                  <li>2. Click the "Summarize" button.</li>
+                  <li>2. Click the &quot;Summarize&quot; button.</li>
                   <li>3. Wait for the video to be summarized.</li>
                   <li>4. Read the summary!</li>
                 </ul>
