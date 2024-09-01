e7c8ada666cf5602ab4e3223d449359a3ddd1243
Bregwin Jogi <bregwinpaul@gmail.com>
2024-08-31 21:05:25 -0400

    Switched from Hugging Face Inference to Gemini

--- src/app/actions.tsx
+++ src/app/actions.tsx
@@ -1,46 +1,34 @@
 'use server'
- 
-
-import { HfInference } from "@huggingface/inference"
-
-const hf = new HfInference('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')
 
 import { YoutubeTranscript } from 'youtube-transcript';
 
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
 interface Sentence {
     text: string;
-    // Other properties if needed
   }
 
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+const prompt = "Please summarize the content of this YouTube video in bullet points. Show it as regular text, DON'T USE MARKDOWN BULLET POINTS. Focus on the key points, main ideas, and any important details or action items mentioned. Ensure the summary is easy to read and highlights the most important aspects of the video.:\n\n";
+
 export async function processVideo (youtubeURL:string): Promise<string> {
-    try {
-      console.log("Processing Video: ", youtubeURL);
+  try {
+    console.log("Processing Video: ", youtubeURL);
 
-      const doc:Sentence[] = await YoutubeTranscript.fetchTranscript(youtubeURL);
-      const combinedText:string = doc.map((sentence:Sentence) => sentence.text).join(" ");
+    const doc:Sentence[] = await YoutubeTranscript.fetchTranscript(youtubeURL);
+    const combinedText:string = doc.map((sentence:Sentence) => sentence.text).join(" ");
+    
+    const result = await model.generateContent(prompt + combinedText);
+    console.log(result.response.text());
 
-      const output = await hf.textGeneration({ //combinedText.substring(0,(combinedText.length)/2)
-        model: 'openchat/openchat-3.5-1210',
-        inputs: 
-        `GPT4 Correct User: Hello
-        <|end_of_turn|>
-        GPT4 Correct Assistant: Hi
-        <|end_of_turn|>
-        GPT4 Correct User: Give a comprehensive overview using bullet points from this video:
-        ${combinedText} 
-        <|end_of_turn|>
-        GPT4 Correct Assistant:`,
-        parameters: { max_new_tokens: 500, return_full_text:false}
-      })
+    return result.response.text()
 
-      console.log("Output: ",output.generated_text) ;
-
-      return output.generated_text
-  
-    } catch (error) {
-      console.error('Error processing video:', error);
-      return "Error Summarizing Video. The video might be too long or has transcription disabled."
-    }
+  } catch (error) {
+    console.error('Error processing video:', error);
+    return "Error Summarizing Video. The video might be too long or has transcription disabled."
+  }
 }
 
 
